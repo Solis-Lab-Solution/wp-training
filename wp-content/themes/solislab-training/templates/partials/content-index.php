bf0b48b708c8d7adadd8c2ed89db5ea3ea36e830
Kevin Nguyen <minhkhoi.nguyen@solislab.com>
2017-07-12 16:38:46 +0700

    Fix article link

--- wp-content/themes/solislab-training/templates/partials/content-index.php
+++ wp-content/themes/solislab-training/templates/partials/content-index.php
@@ -1,6 +1,6 @@
 <article class="article article--index">
 	<h3 class="article__title">
-		<a href="article__link"><?php the_title(); ?></a>
+		<a class="article__link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 	</h3>
 	<div class="article__content">
 		<?php the_content(); ?>
--- wp-content/themes/solislab-training/templates/partials/content-none.php
+++ wp-content/themes/solislab-training/templates/partials/content-none.php
@@ -1,3 +1,4 @@
 <div class="article article--404">
+	<h1 class="article__title">No Post Found</h1>
 	<p>There is no post to display.</p>
 </div>
